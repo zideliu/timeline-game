c0a4552f1031826a1c47346f71d65eb5acf192c0
zideliu <1281663983@qq.com>
2020-06-28 22:28:26 +0800

    Updates

--- precache-manifest.884ddff3f27c8ef7809f39587c729efc.js
+++ precache-manifest.884ddff3f27c8ef7809f39587c729efc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "091c8326253fed0f264f17b77be69761",
+    "revision": "307d59a369d1cc8fb19620f160779506",
     "url": "/timeline-game/index.html"
   },
   {
-    "revision": "11c389dad269a7caef37",
+    "revision": "a547da215cdcae782fa1",
     "url": "/timeline-game/static/css/main.eab24d3c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/timeline-game/static/js/2.579be780.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "11c389dad269a7caef37",
-    "url": "/timeline-game/static/js/main.2516a001.chunk.js"
+    "revision": "a547da215cdcae782fa1",
+    "url": "/timeline-game/static/js/main.df7fab0b.chunk.js"
   },
   {
     "revision": "479c0000c3d0b6b36a7f",
