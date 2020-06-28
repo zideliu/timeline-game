c473eb81557d6bc547a27d7c8b19eae0e7f8dcd8
zideliu <34944964+zideliu@users.noreply.github.com>
2020-06-28 22:11:34 +0800

    Update index.js

--- src/GamePine/index.js
+++ src/GamePine/index.js
@@ -222,7 +222,7 @@ function GamePine (props) {
     console.log('props', props)
     // 将canvas和容器尺寸匹配
     document.querySelector(
-      '#gamePine canvas').style = `zoom:${780 /
+      '#gamePine canvas').style = `zoom:${props.width /
     initSetting.width}`
 
     const bump = new Bump(PIXI)// 碰撞对象
